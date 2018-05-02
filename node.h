ca2c23716f09b95c98f92d7ede64e3bcf21eecf9
vathana-to <vathana.to@outlook.com>
2018-05-01 21:19:38 -0700

    Fix constructor data types

--- node.h
+++ node.h
@@ -6,7 +6,7 @@ using namespace std;
 class Node {
   
   private:
-    char letter = '\0';
+    unsigned char letter = '\0';
     int count = 0;
     Node *left = nullptr;
     Node *right = nullptr;
@@ -14,8 +14,11 @@ class Node {
   
   public:
     Node() {}
-    Node (int new_letter, int new_count, Node *new_left, Node *new_right, string new_enc) :
+    Node (unsigned char new_letter, int new_count, Node *new_left, Node *new_right, string new_enc) :
         letter(new_letter), count(new_count), left(new_left), right(new_right), enc(new_enc) {}
+    Node (unsigned char new_letter, int new_count) : letter(new_letter), count(new_count) {}
+    Node (unsigned char new_letter, int new_count, Node *new_left, Node *new_right) :
+        letter(new_letter), count(new_count), left(new_left), right(new_right) {}
     
     char get_letter() {
         return letter;
